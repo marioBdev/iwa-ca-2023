e0f6eb0fb8e6573ec6cc1610842522322a43c7a1
marioBdev <bacana.vip@gmail.com>
2023-01-02 16:08:34 +0000

    Changing MongoDB access

--- loaders/mongoose.js
+++ loaders/mongoose.js
@@ -1,21 +1,18 @@
 const mongoose = require('mongoose')
-
-const MONGODB_URL = process.env
+require('dotenv').config()
+const  MONGODB_key = process.env.MONGODB_URL
+console.log(MONGODB_key)
 
 exports.connect = () => {
-	mongoose.conect(MONGOB_URL, {
-		// These 2 lines is just required form this in-build method node updating required
-		useNewParser: true,
-		useUnifielldTopology: true
+	mongoose.connect(MONGODB_key, {
+		// This line is just required form this in-build method Node updating required
+		useNewUrlParser: true
 	})
 	.then(console.log('DB connected succssessfully'))
 	.catch((error) => {
 	console.log('DB connections has Failed')
 	console.log(error)
-	// there is a "0" log from MongoDB when all is ok or "1" when there is an issue so ir closes the connection
+	// There is a "0" log from MongoDB when all is ok or "1" when there is an issue so it closes the DB connection
 	process.exit(1)
 	})
 }
-
-
-// await mongoose.connect('mongodb+srv://Mario2019451:CcTMario2019451@cluster0.mpchy.mongodb.net/jobs');
